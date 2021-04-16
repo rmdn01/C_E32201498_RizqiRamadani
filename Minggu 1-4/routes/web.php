fa0348e0efbebcf33984f574b5866e8e55e57e0e
Rizqi Ramadani <rizqiramadani232@gmail.com>
2021-04-16 23:05:31 +0700

    Tugas Minggu 1-4

---
+++ Minggu 1-4/routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Route,
+	App\Http\Controllers\danicontroller;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+	return ('Helo World');
+});
+
+Route::get('/belajar', function () {
+	echo '<h1>Hello World</h1>';
+	echo '<p>Sedang Belajar Laravel</p>';
+});
+
+Route::get('/page/{nomor}', function ($nomor) {
+	return 'Ini Halaman ke' . $nomor;
+});
+
+Route::get('/gambar', function () {
+	return view('gambar');
+});
+
+Route::get('user',[danicontroller::class, 'index']);
+
+Route::resource('user', danicontroller::class);
+
+Route::get("/dani", function(){
+	return view("dani");
+});
