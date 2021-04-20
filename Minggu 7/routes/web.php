f8d891cda817c99bbd0faf2f37d40ad6cbcccf73
Rizqi Ramadani <rizqiramadani232@gmail.com>
2021-04-20 09:26:29 +0700

    Tugas Minggu 7

--- Minggu 7/routes/web.php
+++ Minggu 7/routes/web.php
@@ -52,3 +52,7 @@ Route::resource('dashboard', DashboardController::class);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => ['web','auth']], function () {
+	Route::resource('dashboard', DashboardController::class);
+});
