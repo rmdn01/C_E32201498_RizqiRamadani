fc806d5761e6049eda19117b0343950d49d2ec72
Rizqi Ramadani <rizqiramadani232@gmail.com>
2021-04-21 10:52:57 +0700

    Tugas Minggu 12

---
+++ Minggu 12/app/Http/Controllers/PendidikanController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request,
+    App\Models\Pendidikan;
+
+class PendidikanController extends Controller
+{
+    public function index()
+    {
+        $pendidikan = Pendidikan::get();
+    	return view('backend.pendidikan.index',compact('pendidikan'));
+    }
+    public function create()
+    {
+    	$pendidikan = null;
+    	return view('backend.pendidikan.create', compact('pendidikan'));
+    }
+    public function store(Request $request)
+    {
+    	Pendidikan::create($request->all());
+
+    	return redirect()->route('pendidikan.index')
+    					->with('success','Data Pendidikan baru telah berhasil disimpan.');
+    }
+    public function edit(Pendidikan $pendidikan)
+    {
+        return view('backend.pendidikan.create', compact('pendidikan'));
+    }
+    public function update(Request $request, Pendidikan $pendidikan)
+    {
+        $pendidikan->update($request->all());
+        return redirect()->route('pendidikan.index')
+                        ->with('success','Pendidikan berhasil diperbaharui.');
+    }
+    public function destroy(Pendidikan $pendidikan)
+    {
+        $pendidikan->delete();
+        return redirect()->route('pendidikan.index')
+                        ->with('success','Data Pendidikan berhasil dihapus.');
+    }
+}
